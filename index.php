302d6508532bf1c29f99c4da2f3e44d2af83015b
JohanLiberet <63211400+johanLib@users.noreply.github.com>
2024-12-17 01:11:55 +0100

    Update the icons toolkit link in index.php

--- index.php
+++ index.php
@@ -6,10 +6,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script
-      src="https://kit.fontawesome.com/64d58efce2.js"
-      crossorigin="anonymous"
-    ></script>
+    <script src="https://kit.fontawesome.com/823a574c14.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="dist/style.css" />
     <title>SignIn | SignUp</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
